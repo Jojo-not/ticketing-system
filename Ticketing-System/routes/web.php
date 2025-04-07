61a15ec33bff9d9846cbff3fccbb33ae9f330f32
Jowen <diezjowenmiguel@gmail.com>
2025-04-07 17:50:27 +0800

    oky daw

--- Ticketing-System/routes/web.php
+++ Ticketing-System/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return inertia('dashboard');
+    return inertia::render('Signin');
+});
+Route::get('/Signin', function () {
+    return inertia::render('Signin');
+});
+Route::get('/Signup', function () {
+    return inertia::render('Signup');
 });
